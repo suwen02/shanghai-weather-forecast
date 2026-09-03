1f420703e7f2f1377abcd7fa1d94ae73165eece7
mei suwen <mei.suwen02@gmail.com>
2026-09-03 01:49:59 -0700

    fix: label precipitation classifier as wet event

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 """机器学习模型模块"""
+from config.precipitation_thresholds import WET_EVENT_LABEL
 from .temperature import TemperaturePredictor
 from .precipitation import PrecipitationPredictor
 from .calibration import CalibrationManager
+
+# Semantic metadata is class-level so legacy pickles can still be loaded while
+# all newly trained/runtime predictors expose the canonical event target.
+PrecipitationPredictor.event_label = WET_EVENT_LABEL
